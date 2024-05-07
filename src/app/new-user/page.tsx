4d49dc9932ad971358f17ac4314cf160883000a2
unknown <g.sankhyan1357@gmail.com>
2024-05-07 16:57:27 -0400

    Set up New User Page

--- src/app/new-user/page.tsx
+++ src/app/new-user/page.tsx
@@ -1,5 +1,37 @@
-const NewUser = () => {
-    return <div>Hello!, New User!</div>
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+import { prisma } from '../utils/db'
+
+const createNewUser = async () => {
+    const user = await currentUser()
+    console.log(user)
+
+    if (user == null) {
+        console.log('User is null')
+        return
+    }
+
+    const match = await prisma.user.findUnique({
+        where: {
+            clerkId: user.id as string,
+        },
+    })
+
+    if (!match) {
+        await prisma.user.create({
+            data: {
+                clerkId: user.id,
+                email: user?.emailAddresses[0].emailAddress,
+            },
+        })
+    }
+
+    redirect('/dashboard')
+}
+
+const NewUser = async () => {
+    await createNewUser()
+    return <div>Hello!, New User! Loading...</div>
 }
 
 export default NewUser
