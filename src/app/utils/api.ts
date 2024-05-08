d0020c2845b55a4407a7738de22ee53bd4812135
unknown <g.sankhyan1357@gmail.com>
2024-05-07 20:02:01 -0400

    Add the test code to manually add matches [Doesn't work]
    This code doesn't work so commented

---
+++ src/app/utils/api.ts
@@ -0,0 +1,16 @@
+// const createURL = (path: any) => {
+//     return window.location.origin + path
+// }
+
+// export const createNewMatch = async () => {
+//     const res = await fetch(
+//         new Request(createURL('/api/analytics'), {
+//             method: 'POST',
+//         }),
+//     )
+
+//     if (res.ok) {
+//         const data = await res.json()
+//         return data.data
+//     }
+// }
