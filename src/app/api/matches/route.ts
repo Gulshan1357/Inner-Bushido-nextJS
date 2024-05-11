350ca766dedeac4767cd93427ac2b50e000c499b
unknown <g.sankhyan1357@gmail.com>
2024-05-11 17:12:27 -0400

    Hotfix for deployment

--- src/app/api/matches/route.ts
+++ src/app/api/matches/route.ts
@@ -1,15 +1,15 @@
-// import { getUserByClerkId } from '@/app/utils/auth'
-// import { prisma } from '@/app/utils/db'
-// import { NextResponse } from 'next/server'
+import { getUserByClerkId } from '@/app/utils/auth'
+import { prisma } from '@/app/utils/db'
+import { NextResponse } from 'next/server'
 
-// export const POST = async () => {
-//     const user = await getUserByClerkId()
-//     const match = await prisma.matchHistroy.create({
-//         data: {
-//             userId: user.id,
-//             score: 83,
-//         },
-//     })
+export const POST = async () => {
+    const user = await getUserByClerkId()
+    const match = await prisma.matchHistroy.create({
+        data: {
+            userId: user.id,
+            score: 83,
+        },
+    })
 
-//     return NextResponse.json({ data: match })
-// }
+    return NextResponse.json({ data: match })
+}
